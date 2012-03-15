eb929b2fb7550847cf86a3d1a59a656a51580e02
Jen wicker Hanni <jeh.wicker@gmail.com>
2012-03-14 21:18:34 -0700

    Added explicit .h references.

--- src/main.c
+++ src/main.c
@@ -20,15 +20,19 @@ void delayms(uint16_t millis) {
 }
 
 int main(void) {
-	DDRB |= 1<<PB0;
-	usart_init();
-	pwm_init();
-	pwm_set(0);
-	char ch;
+	DDRB |= 1<<PB0; // data direction register, PB0 pin, <avr/io.h>
+	usart_init(); // <core/usart.h>
+	pwm_init(); // <core/pwm.h>
+	pwm_set(0); // <core/pwm.h>
+	char ch; // because we send chars
 
 	while(1) {
+		// usart_recv_fifo() <core/usart.h>
+		// char_fifo_is_empty() <utils/char_fifo.h>
 		if(char_fifo_is_empty(usart_recv_fifo()))
+			// char_fifo_pop() <core/usart.h>
 			char_fifo_pop(usart_recv_fifo(), &ch);
+			// usart_send_char(); <core/usart.h>
 			usart_send_char(ch);
 	}
 	return 0;
